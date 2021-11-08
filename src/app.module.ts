2ff6a263f1a440e02cac8905390dce14588494f8
Tarcisio Corte <tarcisiocorte@gmail.com>
2021-11-08 17:17:24 -0300

    updated modified files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { UsersModule } from './app/users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.TYPEORM_CONNECTION,
-      host: process.env.TYPEORM_HOST,
-      port: process.env.TYPEORM_PORT,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      synchronize: true,
-    } as TypeOrmModuleOptions),
-    UsersModule,
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
