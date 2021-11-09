c198230e4c9ba49c80b5852400edbafb4dc16f95
Tarcisio Corte <tarcisiocorte@gmail.com>
2021-11-09 14:29:22 -0300

    update events controller

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
+import { CreateEventDto } from './create-event.dto';
+import { UpdateEventDto } from "./update-event.dto";
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return [
+      { id: 1, name: 'First event' },
+      { id: 2, name: 'Second event' }
+    ];
+  }
+
   @Get(':id')
   findOne(@Param('id') id) {
-    return id
+    return { id: 1, name: 'First event' }
   }
+
   @Post()
-  create() {}
-  @Patch()
-  update() {}
-  @Delete()
-  remove() {}
+  create(@Body() input: CreateEventDto) {
+    return input;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id, @Body() input: UpdateEventDto) {
+    return input;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id) { }
 }
