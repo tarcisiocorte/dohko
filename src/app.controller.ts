f0bec7f3f5ab04332776f75012364834f61f9933
Tarcisio Corte <tarcisiocorte@gmail.com>
2021-11-10 18:05:50 -0300

    add TypeOrmModule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
@@ -12,6 +12,6 @@ export class AppController {
 
   @Get('/bye')
   getBye() {
-    return 'Bye!';
+    return "Bye!";
   }
 }
